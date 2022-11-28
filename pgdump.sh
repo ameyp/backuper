627835683aa4845bdc2093bab70810a6c647a893
Amey Parulekar <amey@wirywolf.com>
2022-11-27 16:51:00 -0800

    Print a message before dumping databases.

--- pgdump.sh
+++ pgdump.sh
@@ -8,9 +8,11 @@ if [ $# -ne 1 ]; then
 fi
 
 DEST_DIR="$1"
+DEST_FILE="${DEST_DIR}/dump.sql"
 
 echo "$DB_HOST:$DB_PORT:*:$DB_USERNAME:$DB_PASSWORD" > $HOME/.pgpass
 chmod 600 $HOME/.pgpass
 
 mkdir -p "${DEST_DIR}"
-pg_dumpall -h $DB_HOST -p $DB_PORT -U "$DB_USERNAME" -f "${DEST_DIR}/dump.sql"
+echo "Dumping postgresql databases to ${DEST_FILE}"
+pg_dumpall -h $DB_HOST -p $DB_PORT -U "$DB_USERNAME" -f "${DEST_FILE}"
